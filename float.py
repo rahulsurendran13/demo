63ac3fde6c3bdac2fd1cb63b8122cb9342c4c0c2
rahulsurendran13 <rahulsyrendran134@gmail.com>
2023-07-11 18:03:11 +0530

    adding py fiels

---
+++ float.py
@@ -0,0 +1,6 @@
+a=(5)
+b=float(a)
+print(b)
+b=10
+c=str(b)
+print(type(c))
---
+++ number.py
@@ -0,0 +1,22 @@
+num=int(input("enter first num"))
+num1=int(input("enter second num"))
+num2=int(input("enter thered num"))
+sum = num+num1+num2
+avg = sum/3
+print("avg is",avg)
+# print(avg("num+num2+num3"))
+
+
+a = 5
+b = 6
+c = 8
+
+sum = a+b+c
+print(sum) 
+dob=int(input("enter your dob"))
+cur=2023-dob
+print(cur)
+
+
+
+
